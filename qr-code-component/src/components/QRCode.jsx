39c8838f3ae58b076e6451ccd4ecddfc50aaa14a
Rotem <rotem.barsela95@gmail.com>
2022-08-14 17:44:05 +0300

    minor changes

--- qr-code-component/src/components/QRCode.jsx
+++ qr-code-component/src/components/QRCode.jsx
@@ -4,10 +4,10 @@ import QRCodeImage from '../assets/images/image-qr-code.png'
 const QRCode = () => {
     return (
         <div className='w-screen h-screen flex justify-center items-center bg-c_light_gray'>
-            <div className='bg-c_white rounded-xl shadow-md flex flex-col gap-5 pb-8 items-center p-3'>
+            <div className='bg-c_white rounded-xl max-w-[280px] shadow-md flex flex-col gap-5 pb-8 items-center p-3'>
                 <img src={QRCodeImage} alt='qr_code' className='w-64 h-64 rounded-xl' />
-                <h3 className='max-w-fit text-center text-xl text-c_dark_blue font-bold'>Improve your front-end skills by building projects</h3>
-                <p className='max-w-fit text-center text-c_grayish_blue'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+                <h3 className='text-center text-xl text-c_dark_blue font-bold'>Improve your front-end skills by building projects</h3>
+                <p className='text-center text-c_grayish_blue'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
             </div>
         </div>
     )
